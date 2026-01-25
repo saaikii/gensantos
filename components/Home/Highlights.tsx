80edeecd0ac17eaf4efe530171bb1f609608ed84
saaikii <saaikii@users.noreply.github.com>
2026-01-25 11:25:00 +0800

    abadni

--- components/Home/Highlights.tsx
+++ components/Home/Highlights.tsx
@@ -10,7 +10,11 @@ const Highlights: React.FC = () => {
 
       {/* Background Decorations */}
       <div className="absolute top-0 right-0 w-1/2 h-full bg-gradient-to-l from-blue-50 to-transparent skew-x-12 transform translate-x-20 opacity-60 pointer-events-none"></div>
-      <div className="absolute bottom-0 left-0 w-96 h-96 bg-yellow-100/40 rounded-full blur-3xl pointer-events-none"></div>
+      <div className="absolute -top-20 -left-20 w-72 h-72 bg-purple-200/60 rounded-full blur-3xl pointer-events-none animate-blob"></div>
+      <div className="absolute -top-20 -right-20 w-80 h-80 bg-cyan-200/60 rounded-full blur-3xl pointer-events-none animate-blob animation-delay-2000"></div>
+      <div className="absolute -bottom-20 -left-20 w-96 h-96 bg-yellow-200/60 rounded-full blur-3xl pointer-events-none animate-blob animation-delay-4000"></div>
+      <div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-lime-200/60 rounded-full blur-3xl pointer-events-none animate-blob animation-delay-4000"></div>
+      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 w-80 h-80 bg-fuchsia-200/60 rounded-full blur-3xl pointer-events-none animate-blob"></div>
 
       <div className="container mx-auto px-6 relative z-10">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 lg:gap-24 items-center">
